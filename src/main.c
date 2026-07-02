55110b90e4b8ad48297ee05f6fa256cde8040ab0
IT26100411 <it26100411@my.sliit.lk>
2026-07-02 22:22:06 +0530

    Implement 15x15 game map with border wall initialization

--- src/main.c
+++ src/main.c
@@ -1,8 +1,45 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main (){
+#define MAP_SIZE 15
 
-    printf("Quest for the Lost Treasure\n");
-    
-    return 0;
+char map[MAP_SIZE][MAP_SIZE]; 
+
+
+void initializeMap(){  // Initializes the 15x15 map and places border walls.
+
+	int i, j;
+
+	for (i = 0; i < MAP_SIZE; i++){
+		for (j = 0; j < MAP_SIZE; j++){
+			if (i == 0 || i == MAP_SIZE - 1 || j == 0 || j == MAP_SIZE - 1){ // Places border walls represented by '#' character.
+				map[i][j] = '#';
+			}
+			else {
+				map[i][j] = ' ';
+			}
+		}
+	}
 }
+
+
+void printMap(){  // Prints the 15X15 map to the console as output.
+	
+	int i, j;
+
+	for (i = 0; i < MAP_SIZE; i++){
+		for (j = 0; j < MAP_SIZE; j++){
+			printf("%c ", map[i][j]);
+		}
+	printf("\n");
+	}
+}	
+
+
+int main (){ // Main function that initializes and prints the map.
+
+	printf("Quest for the Lost Treasure\n");
+	initializeMap();
+	printMap();
+
+	return 0;
+}    
